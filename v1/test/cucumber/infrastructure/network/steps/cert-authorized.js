bfb5845da673b79616cc9c7bae86b2fc4d6ee37f
Neal Gamradt <neal.gamradt@turner.com>
2022-08-23 01:18:39 -0400

    fix: Correcting timeout configuration (NGTSK-147)

--- v1/test/cucumber/infrastructure/network/steps/cert-authorized.js
+++ v1/test/cucumber/infrastructure/network/steps/cert-authorized.js
@@ -9,7 +9,9 @@ When('we request the health check URL and receive the certificate response', asy
   const domain = process.env.SERVICE_DOMAIN;
   const hc = process.env.SERVICE_HEALTH_CHECK;
   const options = {
-    timeout:10000
+    timeout:{
+      request:10000
+    }
   };
   try {
     global.response = await got.get("https://"+domain+hc,options);
--- v1/test/cucumber/infrastructure/network/steps/tls.js
+++ v1/test/cucumber/infrastructure/network/steps/tls.js
@@ -9,7 +9,9 @@ When('we request the health check URL using the {string} protocol', async (proto
   const domain = process.env.SERVICE_DOMAIN;
   const hc = process.env.SERVICE_HEALTH_CHECK;
   const options = {
-    timeout:10000,
+    timeout:{
+      request:10000
+    },
     secureProtocol: protocol
   };
   try {
